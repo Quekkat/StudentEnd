a6052c9e1ba1b838aca63457f2c1be6d0e9ef67d
Quekkat <captlucas07@gmail.com>
2025-05-20 07:27:06 +0800

    backend core

--- backend/src/controllers/authcontrollers.js
+++ backend/src/controllers/authcontrollers.js
@@ -36,9 +36,34 @@ export const signup = async (req,res)=>{
         res.status(500).json({message:"Internal server Error"});
     }
 }
-export const login =(req,res)=>{
-    res.send("login route");
+
+export const login =async (req,res)=>{
+    //Handles login, returns student data and jwt
+        const {USN, USNPassword} = req.body;
+    try{
+        const student = await Student.findOne({USN});
+        if (!student) return res.status(400).json({message:"Invalid credentials"});
+        const correctPassword = await bcrypt.compare(USNPassword, Student.lmsURNPassword);
+        if(!correctPassword) return res.status(400).json({message:"Invalid credentials"});
+        generateToken(student._id,res);
+        res.status(200).json({
+            id: student._id,
+            fname: student.firstName,
+            lname: student.lastName,
+            section: student.section,
+            level: student.yearOrLevel
+        });
+    }catch(error){
+        console.log("Error in login controller:", error.message);
+        res.status(500).json({message:"Internal server Error"});
+    }
 }
 export const logout =(req,res)=>{
-    res.send("logout route");
+    try{
+        res.cookie("tokencookie","",{maxAge:0});
+        res.status(200).json({message:"logged out"});
+    }catch(error){
+        console.log("Error in logout controller:", error.message);
+        res.status(500).json({message:"Internal server Error"});
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import dotenv from "dotenv"
 import { connectDB } from "./lib/database.js";
+import cookieparser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
+app.use(cookieparser());
 
 //Sign in & signup routes
 app.use("/api/auth", authRoutes);
